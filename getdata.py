411f4826d2b3f0295b5717579806e8aa886e2a39
Simon Parent <sboparen@gmail.com>
2016-10-04 18:21:00 -0400

    Cleaner script to get repo JSON data.

---
+++ getdata.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+import json
+import os
+import requests
+
+def update_repos():
+    response = requests.get('https://api.github.com/users/sboparen/repos')
+    response.raise_for_status()
+    data = json.loads(response.content)
+    data = [{'name': x['name'], 'description': x['description']} for x in data]
+    data.sort(key=lambda x: x['name'])
+    with open('_data/repos.json', 'w') as f:
+        f.write(json.dumps(data, indent=4, sort_keys=True))
+
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(__file__))
+    update_repos()
