ce314db5a9b87c5ed05c831c63bafc968add337b
Simon Parent <sboparen@gmail.com>
2016-10-05 16:11:17 -0400

    Fix broken links on github page.

--- _getdata.py
+++ _getdata.py
@@ -7,7 +7,8 @@ def update_repos():
     response = requests.get('https://api.github.com/users/sboparen/repos')
     response.raise_for_status()
     data = json.loads(response.content)
-    data = [{'name': x['name'], 'description': x['description']} for x in data]
+    keys = ['name', 'description', 'html_url']
+    data = [{k: x[k] for k in keys} for x in data]
     data.sort(key=lambda x: x['name'])
     with open('_data/repos.json', 'w') as f:
         f.write(json.dumps(data, indent=4, sort_keys=True))
