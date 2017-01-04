c7caa7753c6f4049ce3bfdd219fc2bdf83bdfc2f
Simon Parent <sboparen@gmail.com>
2017-01-04 17:14:22 -0500

    Simplify the page about my github repos.

--- _getdata.py
+++ _getdata.py
@@ -3,15 +3,47 @@ import json
 import os
 import requests
 
+categories = [
+    ('Software I Use', [
+        'env',
+        'dwm',
+        'gmail-snooze',
+        'ren',
+        ]),
+    ('Projects', [
+        '1k-codec',
+        'vrdemo',
+        ]),
+    ('Video Game Related', [
+        'openitg',
+        'spacechem',
+        'stepmania',
+        'tis100',
+        ]),
+    ('This Website', [
+        'sboparen.github.io',
+        'resume',
+        ]),
+]
+
 def update_repos():
     response = requests.get('https://api.github.com/users/sboparen/repos')
     response.raise_for_status()
     data = json.loads(response.content)
-    keys = ['name', 'description', 'html_url']
-    data = [{k: x[k] for k in keys} for x in data]
-    data.sort(key=lambda x: x['name'])
+    data = {repo['name']: repo for repo in data}
+    output = []
+    for title, reponames in categories:
+        repos = []
+        for reponame in reponames:
+            assert reponame in data, '%s not found' % reponame
+            repo = data[reponame]
+            del data[reponame]
+            keys = ['name', 'description', 'html_url']
+            repos.append({k: repo[k] for k in keys})
+        output.append({'title': title, 'repos': repos})
+    assert len(data) == 0, 'uncategorized repos: %s' % data.keys()
     with open('_data/repos.json', 'w') as f:
-        f.write(json.dumps(data, indent=4, sort_keys=True))
+        f.write(json.dumps(output, indent=4, sort_keys=True))
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(__file__))
